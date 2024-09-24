9cb162efceec7757734753852f22c2e07452fb11
Jorim <jorimpablico2004@gmail.com>
2024-09-25 00:21:24 +0800

    1. DisplayHelloWorldActivity

---
+++ app/src/main/java/com/example/androidjavacomponents/JavaAndroidComponents/DisplayHelloWorldActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidjavacomponents.JavaAndroidComponents;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.androidjavacomponents.R;
+
+public class DisplayHelloWorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_display_hello_world);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
